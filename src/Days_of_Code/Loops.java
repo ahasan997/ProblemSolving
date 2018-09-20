7e2096648f36b9778c8d260f5b485bc90030e063
selvator <ahasan997@gmail.com>
2018-09-20 10:06:00 +0600

    New Class Operators

---
+++ src/Days_of_Code/Loops.java
@@ -0,0 +1,29 @@
+package Days_of_Code;
+import java.util.*;
+
+public class Loops {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int n = scanner.nextInt();
+
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int x = 1;
+        int y = 0;
+
+        do{
+            y = n * x;
+            System.out.println(n+" x "+x+" = "+y);
+            x++;
+        }
+        while (x <= 10);
+
+
+        scanner.close();
+
+    }
+
+}
