2fd8b43d761517fec9adc583c5911271734c0d4c
neo <neo@localhost>
2018-10-02 12:29:53 +0600

    Solution "PostorderTraversal" added

---
+++ src/Data_Structures/Tree/PostorderTraversal.java
@@ -0,0 +1,42 @@
+package Data_Structures.Tree;
+
+import java.util.Scanner;
+
+public class PostorderTraversal {
+    public static void preOrder(Node root) {
+        if(root == null) {
+            return;
+        }
+        preOrder(root.left);
+        preOrder(root.right);
+        System.out.print(root.data+" ");
+    }
+
+    public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        Node root = null;
+        while (t-- > 0) {
+            int data = sc.nextInt();
+            root = insert(root, data);
+        }
+        sc.close();
+        preOrder(root);
+    }
+}
