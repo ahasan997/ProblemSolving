9b199cff288eb76822eec637dd38353b3b5ed9c8
ahasan997 <ahasan997@gmail.com>
2019-12-28 17:49:48 +0600

    updated test classes

--- src/TestClasses/MyClass3.java
+++
@@ -1,9 +0,0 @@
-package TestClasses;
-
-public class MyClass3 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Ahasan");
-    }
-}
